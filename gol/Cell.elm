d3ad0e146198cab23de5c1c14762c14093d07c49
Federico Ragona <federico.ragona@gmail.com>
2016-08-04 01:38:07 +0200

    Implement GoL

--- gol/Cell.elm
+++ gol/Cell.elm
@@ -1,4 +1,4 @@
-module Cell exposing (Model, Msg, init, update, view)
+module Cell exposing (Model, Msg(Evolve), init, update, view)
 
 import Svg exposing (..)
 import Svg.Attributes exposing (..)
@@ -6,22 +6,46 @@ import Svg.Attributes exposing (..)
 type alias Model =
   { x : Int
   , y : Int
+  , z : Int
   , alive : Bool
   }
 
-init x y =
-  Model x y True
+init x y z alive =
+  Model x y z alive
 
-type Msg = Die
+type Msg = Evolve (List Model)
 
+reproduce model = { model | alive = True }
+
+die model = { model | alive = False }
+
+-- just for readability
+survive model = model
+
+update : Msg -> Model -> Model
 update msg model =
   case msg of
-    Die ->
-      { model | alive = False }
+    Evolve neighbours ->
+      case neighbours of
+        [] -> die model -- undercrowded
+        h::[] -> die model -- undercrowded
+        n ->
+          case (List.length n) of
+            2 -> survive model
+            3 -> reproduce model
+            _ -> die model -- overcrowded
 
 view : Model -> Svg a
 view model =
   let
     color = if (model.alive) then "green" else "red"
   in
-    circle [ cx (toString model.x), cy (toString model.y), r "10", fill color ] []
+    rect
+    [ x (toString model.x)
+      , y (toString model.y)
+      , width (toString model.z)
+      , height (toString model.z)
+      , fill color
+      , stroke "black"
+      , strokeWidth "2px"
+    ] []
--- gol/GoL.elm
+++ gol/GoL.elm
@@ -1,15 +1,18 @@
 import Cell
-
-import Html exposing (Html, div, span, text)
+import Html exposing (Html, button, div, span, text)
 import Html.App as App
+import Html.Events exposing (onClick)
 import Svg exposing (svg)
 import Svg.Attributes exposing (height, viewBox, width)
-import Time exposing (every, second, Time)
+import Time exposing (Time, every, second)
 
 type alias Model =
   { generation : Int
-  , cell : Cell.Model
+  , cells : List Cell.Model
+  , paused : Bool
   }
+
+type Msg = NextGen Time | Pause
 
 main =
   App.program {
@@ -21,24 +24,62 @@ main =
 
 init : (Model, Cmd Msg)
 init =
-  (Model 0 (Cell.init 50 50), Cmd.none)
+  let
+    boardSize = 4
+    cellSize = 30
+    board =
+      List.concatMap
+        (\i ->
+          List.map
+            (\j ->
+              let alive = (i == 1 || j == 4)
+              in (Cell.Model (i * cellSize) (j * cellSize) cellSize alive))
+            [0..boardSize])
+        [0..boardSize]
+  in
+    (Model 0 board False, Cmd.none)
 
-type Msg = NextGen Time
+aliveNeighboursOf : Cell.Model -> List Cell.Model -> List Cell.Model
+aliveNeighboursOf cell cells =
+  List.filter
+    (\other ->
+      other.alive == True
+      && cell /= other
+      && (abs cell.x - other.x) <= cell.z
+      && (abs cell.y - other.y) <= cell.z)
+    cells
+
+updateCell : Cell.Model -> List Cell.Model -> Cell.Model
+updateCell cell cells =
+  let
+    neighbours = aliveNeighboursOf cell cells
+  in
+    (Cell.update (Cell.Evolve neighbours) cell)
 
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
-    NextGen t ->
-      ({ model | generation = model.generation + 1 }, Cmd.none)
+    Pause ->
+      ({ model | paused = (not model.paused) }, Cmd.none)
+    NextGen _ ->
+      ({ model |
+        generation = model.generation + 1
+        , cells = List.map (\cell -> (updateCell cell model.cells)) model.cells
+        },
+        Cmd.none)
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
-  Time.every second NextGen
+  if (model.paused || List.all (\x -> not x.alive) model.cells) then
+    Sub.none
+  else
+    Time.every second NextGen
 
-view : Model -> Html a
+view : Model -> Html Msg
 view model =
   div [] [
     span [] [text ("Generation: " ++ (toString model.generation))],
+    button [onClick Pause] [text "Pause/Resume"],
     svg [ viewBox "0 0 300 300", height "300px", width "300px" ]
-      [ (Cell.view model.cell) ]
+      (List.map Cell.view model.cells)
   ]
