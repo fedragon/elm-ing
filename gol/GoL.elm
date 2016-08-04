1c2e1a3e0bfda3148cb1b24ec87ebb019e1d11fb
Federico Ragona <f.ragona@thenewmotion.com>
2016-08-04 12:47:10 +0200

    Generate random GoL board

--- gol/GoL.elm
+++ gol/GoL.elm
@@ -2,6 +2,7 @@ import Cell
 import Html exposing (Html, button, div, span, text)
 import Html.App as App
 import Html.Events exposing (onClick)
+import Random
 import Svg exposing (svg)
 import Svg.Attributes exposing (height, viewBox, width)
 import Time exposing (Time, every, second)
@@ -12,7 +13,10 @@ type alias Model =
   , paused : Bool
   }
 
-type Msg = NextGen Time | Pause
+type Msg =
+  NextGen Time
+  | Pause
+  | RaiseFromTheDead (List (Int, Int))
 
 main =
   App.program {
@@ -21,6 +25,16 @@ main =
     subscriptions = subscriptions,
     view = view
   }
+
+raiseFromTheDead : Int -> Int -> Cmd Msg
+raiseFromTheDead total boardSize =
+  Random.generate
+    RaiseFromTheDead
+    (Random.list
+      total
+      (Random.pair
+        (Random.int 1 boardSize)
+        (Random.int 1 boardSize)))
 
 init : (Model, Cmd Msg)
 init =
@@ -32,12 +46,14 @@ init =
         (\i ->
           List.map
             (\j ->
-              let alive = (i == 1 || j == 4)
-              in (Cell.Model (i * cellSize) (j * cellSize) cellSize alive))
+              let
+                x = i * cellSize
+                y = j * cellSize
+              in (Cell.Model x y cellSize False))
             [0..boardSize])
         [0..boardSize]
   in
-    (Model 0 board False, Cmd.none)
+    (Model 0 board True, raiseFromTheDead 5 boardSize)
 
 aliveNeighboursOf : Cell.Model -> List Cell.Model -> List Cell.Model
 aliveNeighboursOf cell cells =
@@ -56,9 +72,23 @@ updateCell cell cells =
   in
     (Cell.update (Cell.Evolve neighbours) cell)
 
+shouldBeAlive cell reborn =
+  List.any
+    (\(x, y) -> cell.x == x * cell.z && cell.y == y * cell.z)
+    reborn
+
 update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
   case msg of
+    RaiseFromTheDead reborn ->
+      ({ model |
+        cells =
+          List.map
+            (\cell ->
+              if (shouldBeAlive cell reborn) then { cell | alive = True } else cell)
+            model.cells,
+            paused = False
+          }, Cmd.none)
     Pause ->
       ({ model | paused = (not model.paused) }, Cmd.none)
     NextGen _ ->
